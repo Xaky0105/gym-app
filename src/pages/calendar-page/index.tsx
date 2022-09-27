680de6ad922faca3c5efd4d4f6d3355a48e467d1
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-09-27 20:07:46 +0300

    creacte component calendar-page

--- src/pages/calendar-page/index.tsx
+++ src/pages/calendar-page/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container } from '../../compound/container'
+import { useAppSelector } from '../../hooks/redux-hook'
+import { getMonth } from '../../utils/dayjs'
+import CalendarHeader from './calendar-header'
+import Month from './month'
 
 const CalendarPage:React.FC = () => {
+    const [currentMonth, setCurrentMonth] = useState(getMonth()) // В стейте двумерный массив
+    const monthIndex = useAppSelector(state => state.month.monthIndex)
+    useEffect(() => {   
+        setCurrentMonth(getMonth(monthIndex))
+    }, [monthIndex])
     return (
-        <div>Calendar-page</div>
+        <Container>
+            <CalendarHeader />
+            <Month month={currentMonth}/>
+        </Container>
+            
     )
 }
 
