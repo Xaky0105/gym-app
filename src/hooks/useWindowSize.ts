a89cd635a308ce2fe7de74cb45ed52a83f312d88
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-09-27 20:12:42 +0300

    create custom hook useWindowSize

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,23 @@
+import {useEffect, useState} from 'react';
+
+export const useWindowSize = () => {
+    const [size, setSize] = useState({
+        windowWidth: window.innerWidth, 
+        windowHeight: window.innerHeight
+    })
+
+    const handleResize = () => {
+        setSize({
+            windowWidth: window.innerWidth, 
+            windowHeight: window.innerHeight
+        })
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize)
+
+        return () => window.removeEventListener('resize', handleResize)
+    }, [size])
+
+    return size
+}
