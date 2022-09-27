2de32078e6d973ed76c87bc8a4003487b9735ae2
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-09-27 20:05:42 +0300

    create component Month

---
+++ src/pages/calendar-page/month/index.tsx
@@ -0,0 +1,24 @@
+import { type Dayjs } from 'dayjs'
+import React from 'react'
+import Day from '../day'
+import styles from './index.module.scss'
+
+interface MonthProps {
+    month: Dayjs[][]
+}
+
+const Month: React.FC<MonthProps> = ({month}) => {
+    return (
+        <div className={styles.wrapper}>
+            {month.map((row: any[], i: number) => (
+                <React.Fragment key={i}>
+                    {row.map((day: Dayjs, index: number) => (
+                        <Day day={day} key={index} row={i}/>
+                    ))}
+                </React.Fragment>
+            ))}
+        </div>
+    )
+}
+
+export default Month
