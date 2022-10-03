7a57e49022646eab1b28539dff5826e4bfdba75f
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-10-03 16:00:18 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 import Layout from './components/layouts'
 import { Route, Routes } from 'react-router-dom'
 import CalendarPage from './pages/calendar-page'
-import TrainingPage from './pages/training-page'
+import AnalyticsPage from './pages/analytics-page'
+import WorkoutsPage from './pages/workouts-page'
+import CreateWorkoutPage from './pages/create-workout-page'
+import { ROUTE_PATH } from './types/route'
 
 const App:React.FC = () => {
     return (
         <Layout>
             <Routes>
-                <Route path='/calendar' element={<CalendarPage />}/>
-                <Route path='/training' element={<TrainingPage />}/>
+                <Route path={ROUTE_PATH.CALENDAR} element={<CalendarPage />}/>
+                <Route path={ROUTE_PATH.WORKOUT} element={<WorkoutsPage />}/>
+                <Route path={ROUTE_PATH.ANALYTICS} element={<AnalyticsPage />}/>
+                <Route path={ROUTE_PATH.CREATE_WORKOUT} element={<CreateWorkoutPage />}/>
+                <Route path={ROUTE_PATH.EDIT_WORKOUT} element={<CreateWorkoutPage />}/>
             </Routes>
         </Layout>
     )
