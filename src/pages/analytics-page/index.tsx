12bf6d777a33d7d00d3f392f2f5c2c4586edea17
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-10-03 16:00:58 +0300

    init analytics-page

---
+++ src/pages/analytics-page/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+const AnalyticsPage = () => {
+    return (
+        <div>
+            Analytic-page
+        </div>
+    )
+}
+
+export default AnalyticsPage
