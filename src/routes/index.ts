e6687812ff9519f9483214eb0b92c9ef10f3ef0b
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-10-06 18:47:34 +0300

    create routes

---
+++ src/routes/index.ts
@@ -0,0 +1,53 @@
+import React from 'react';
+import AnalyticsPage from '../pages/analytics-page';
+import CalendarPage from '../pages/calendar-page';
+import CreateWorkoutPage from '../pages/create-workout-page';
+import { LoginPage } from '../pages/login-page';
+import NotFoundPage from '../pages/not-found-page';
+import { RegisterPage } from '../pages/register-page';
+import WorkoutsPage from '../pages/workouts-page';
+import { ROUTE_PATH } from './../types/route';
+
+type Route = {
+    path: string,
+    Component: React.FC
+}
+
+export const publicRoutesArr: Route[] = [
+    {
+        path: ROUTE_PATH.LOGIN,
+        Component: LoginPage
+    },
+    {
+        path: ROUTE_PATH.REGISTER,
+        Component: RegisterPage
+    },
+    {
+        path: ROUTE_PATH.NOT_FOUND,
+        Component: NotFoundPage
+    },
+]
+
+export const privateRouteArr: Route[] = [
+    {
+        path: ROUTE_PATH.CALENDAR,
+        Component: CalendarPage
+    },
+    {
+        path: ROUTE_PATH.ANALYTICS,
+        Component: AnalyticsPage
+    },
+    {
+        path: ROUTE_PATH.CREATE_WORKOUT,
+        Component: CreateWorkoutPage
+    },
+    {
+        path: ROUTE_PATH.EDIT_WORKOUT,
+        Component: CreateWorkoutPage
+    },
+    {
+        path: ROUTE_PATH.WORKOUT,
+        Component: WorkoutsPage
+    },
+
+]
