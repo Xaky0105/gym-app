d0369afc9e37f765b6dc5c9af6f3235eed89ab7a
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-10-06 18:49:34 +0300

    create error component

---
+++ src/components/errors/auth-error/index.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react'
+import styles from './index.module.scss'
+
+type AuthErrorPropsType = {
+    errorMessage: string
+}
+
+export const AuthError:FC<AuthErrorPropsType> = ({errorMessage}) => {
+    const showFormatError = () => {
+        switch (errorMessage) {
+            case "Firebase: Error (auth/user-not-found).":
+                return 'Неверный логин или пароль'
+            case "Firebase: Error (auth/wrong-password).":
+                return 'Неверный логин или пароль'
+            case "Firebase: Error (auth/email-already-in-use).":
+                return 'Этот email уже используется'
+            default:
+                return 'Что-то пошло не так';
+        }
+    }
+    return (
+        <div className={styles.error}>
+            <p className={styles.message}>{showFormatError()}</p>
+        </div>
+    )
+}
