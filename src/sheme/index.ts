3e8188b45ca4b8a7442c7542b096a792f248b84f
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-10-06 18:56:11 +0300

    update sheme

--- src/sheme/index.ts
+++ src/sheme/index.ts
@@ -1,7 +1,36 @@
-import { object, string } from 'yup';
+import { object, ref, string } from 'yup';
 
 export const createWorkoutSchema = object({
     workoutName: string()
     .min(4, 'Поле должно содержать минимум 4 символа')
     .required('Обязательное поле'),
+});
+
+const a = ['123', '123']
+
+export const loginSchema = object({
+    email: 
+        string()
+        .email('Не корректно введенный email')
+        .notOneOf(a, 'Такой эмейл уже есть')
+        .required('Необходимо заполнить поле'),
+    password: 
+        string()
+        .min(8, 'Длина пароля должна быть не менее 8 символов')
+        .required('Необходимо заполнить поле'),
+});
+
+export const registerSchema = object({
+    email: 
+        string()
+        .email('Не корректно введенный email')
+        .required('Необходимо заполнить поле'),
+    password: 
+        string()
+        .min(8, 'Длина пароля должна быть не менее 8 символов')
+        .required('Необходимо заполнить поле'),
+    confirmPassword:
+        string()
+        .oneOf([ref('password')], 'Пароли не совпадают')
+        .required('Необходимо заполнить поле') 
 });
