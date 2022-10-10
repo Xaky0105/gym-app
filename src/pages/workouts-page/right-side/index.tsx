4914e80af4e35997c3989fb31dace5d6d91eb24f
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-10-10 13:49:15 +0300

    install lodash, delete custom utils for objects

--- src/pages/workouts-page/right-side/index.tsx
+++ src/pages/workouts-page/right-side/index.tsx
@@ -1,4 +1,5 @@
 import {FC} from 'react'
+import _ from 'lodash'
 import { useAppSelector } from '../../../hooks/redux-hook'
 import { getWorkouts } from '../../../store/selectors'
 import styles from './index.module.scss'
@@ -14,7 +15,7 @@ const RightSide:FC<RightSideTypeProps> = ({ workoutId }) => {
         <div className={styles.rightSide}>
             <h2 className={styles.title}>Упражнения</h2>
             <ul className={styles.exerciseList}>
-                {exercises && exercises.map((exercise) => (
+                {exercises && _.toArray(exercises).map((exercise) => (
                     <li key={exercise.id} className={styles.exerciseItem}>
                         {exercise.name}
                     </li>
