0bbc32a7d8f1d26470dd3ee9c203167799d85241
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-10-16 21:32:37 +0300

    add new selectors

--- src/store/selectors/index.ts
+++ src/store/selectors/index.ts
@@ -1,12 +1,13 @@
-import { createSelector } from '@reduxjs/toolkit';
-import { Exercise } from '../../types/workout';
-import { RootState } from './../index';
+import { createSelector } from '@reduxjs/toolkit'
+import { Exercise } from '../../types/workout'
+import { RootState } from './../index'
 
 export const getMonthIndex = (state: RootState) => state.month.monthIndex
 
 export const getSelectedDay = (state: RootState) => state.day.daySelected
 
 export const getIsOpenModalWorkout = (state: RootState) => state.modal.modaleWorkoutIsOpen
+export const getIsOpenConfirmModale = (state: RootState) => state.modal.confirmModaleIsOpen
 export const getStepWorkoutModale = (state: RootState) => state.modal.stepWorkoutModale
 export const getTempIdWorkout = (state: RootState) => state.modal.idSelectedWorkout
 export const getTempIdExercise = (state: RootState) => state.modal.idSelectedExercise
@@ -20,7 +21,16 @@ export const getExerciseList = (state: RootState) => state.workout.exerciseList
 export const getUser = (state: RootState) => state.user.user
 export const getUserError = (state: RootState) => state.user.error
 export const getUserIsLoading = (state: RootState) => state.user.isLoading
-export const getUserEmail = (state: RootState) => state.user.user?.email
+export const getUserEmail = (state: RootState) => state.user.user!.email
+export const getUserPhoto = (state: RootState) => state.user.user!.photoURL
+
+export const getUserPhotoByEmail = createSelector(
+    getUserEmail,
+    getUserPhoto,
+    (email, photo) => {
+        return photo ? photo : `https://ui-avatars.com/api/?size=128&name=${email}&font-size=0.53&background=ccc&color=fff&rounded=true`
+    }
+)
 
 export const getWorkoutById = createSelector(
     getWorkoutsFromCalendar,
