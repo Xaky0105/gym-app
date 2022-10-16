eec55b96340597bf9c39de9087a26a7f98545c16
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-10-16 21:36:42 +0300

    add new types and change constants

--- src/types/workout.ts
+++ src/types/workout.ts
@@ -23,3 +23,14 @@ export type ExerciseListType = {
 export type UserWorkoutsStateType = {
     [key: string]: Workout
 }
+
+export enum DELETE_WORKOUT_FROM_CALENDAR {
+    ONLY_ONE = 'ONLY_ONE',
+    THIS_AND_NEXT = 'THIS_AND_NEXT'
+}
+export enum HOW_TO_REPEAT {
+    DONT_REPEAT = 'DONT_REPEAT',
+    ONCE_A_WEEK = 'ONCE_A_WEEK',
+    INTERVAL = "INTERVAL",
+    EVERY_DAY = 'EVERY_DAY'
+}
