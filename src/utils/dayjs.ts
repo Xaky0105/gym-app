4acda60b95b76478eccc1b29810d18781ef80763
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-10-16 21:29:54 +0300

    add new utils

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,10 +1,12 @@
 import dayjs from "dayjs";
 import ruLocale from'dayjs/locale/ru'
 import weekday from 'dayjs/plugin/weekday' // Для установки дня недели в соответсвии с локалью
+import { DAY_FORMAT } from "../types/day";
+import { HOW_TO_REPEAT, Workout } from "../types/workout";
 
 dayjs.extend(weekday)
 
-export const getMonth = (month = dayjs().month()) => {
+export const getMonthMatrix = (month = dayjs().month()) => {
     const year = dayjs().locale(ruLocale).year();
     const firstDayOfTheMonth = dayjs(new Date(year, month, 1)).locale(ruLocale).weekday()
     let currentMonthCount = 0 - firstDayOfTheMonth;
@@ -23,7 +25,30 @@ export const getYear = (monthIndex: number): string => {
 }
 
 export const getCurrentDay = () => {
-    return dayjs().locale(ruLocale).format('DD/MM/YYYY')
+    return dayjs().locale(ruLocale).format(DAY_FORMAT.YYYY_MM_DD)
+}
+
+export const getWorkoutsDates = (type: HOW_TO_REPEAT, workout: Workout, repeatInterval?: number) => {
+    const workoutDate = workout.date!
+    const dateArr: string[] = []
+    const limit = 20214000000 // Примерно пол года. Потом сделать точное время
+    if (type === HOW_TO_REPEAT.ONCE_A_WEEK) {
+        const weeks = 30
+        for (let i = 0; i < weeks; i++) {
+            dateArr.push(dayjs(workoutDate).add(i , 'week').format(DAY_FORMAT.YYYY_MM_DD))
+        } 
+    } else if (type === HOW_TO_REPEAT.INTERVAL) {
+        let diff = 0
+        let day = 0
+        while (diff < limit) {
+            const lastDate = dateArr[dateArr.length - 1]
+            const firstDate = dateArr[0]
+            diff = dayjs(lastDate).diff(dayjs(firstDate))
+            dateArr.push(dayjs(workoutDate).add(day , 'day').format(DAY_FORMAT.YYYY_MM_DD))
+            day = day + (repeatInterval! + 1)
+        }
+    }
+    return dateArr
 }
 
 export const daysNotIncludedInCurrentMonth = (monthIndex: number) => {
--- src/utils/workout.ts
+++ src/utils/workout.ts
@@ -1,9 +1,11 @@
-import { Workout, Exercise } from './../types/workout';
+import { Workout, Exercise, UserWorkoutsStateType } from './../types/workout';
 import { v4 as uuidv4 } from 'uuid';
 import _ from 'lodash'
-// import { getArrFromObject } from './object';
 
 type GenerateWorkout = ( daySelected: string, workout: Workout) => Workout
+type GetWorkoutsIdToDelete = (workoutsOnTheCalendar: UserWorkoutsStateType, id: string) => string[]
+type GenerateArrWorkoutsForCalendars = (workout: Workout, workoutDates: string[]) => Workout[]
+type ConvertWorkoutDateToNumber = (workoutDate: string) => number
 
 export const generateWorkout: GenerateWorkout = (daySelected, workout) => {
     const arrExercise = _.toArray(workout.exercises)
@@ -21,4 +23,28 @@ export const generateWorkout: GenerateWorkout = (daySelected, workout) => {
         exercises
     }
     return workoutData
+}
+
+export const generateArrWorkoutsForCalendars: GenerateArrWorkoutsForCalendars = (workout, workoutDates) => {
+    return workoutDates.map((date) => ({...workout, id: uuidv4(), date}))
+}
+
+export const convertWorkoutDateToNumber: ConvertWorkoutDateToNumber = (workoutDate) => {
+    const splitDate = workoutDate.split('-')
+    const [ year, month, day ] = splitDate
+    return new Date().setFullYear(+year, +month, +day)
+}
+
+export const getArrWorkoutsIdToDelete: GetWorkoutsIdToDelete = (workoutsOnTheCalendar, id) => {
+    const selectWorkout = workoutsOnTheCalendar[id]
+    const selectWorkoutDate = convertWorkoutDateToNumber(selectWorkout.date!)
+    return _.toArray(workoutsOnTheCalendar)
+        .filter((workout: Workout) => {
+            const workoutDate = convertWorkoutDateToNumber(workout.date!)
+            return (
+                workoutDate >= selectWorkoutDate &&
+                workout.workoutName === selectWorkout.workoutName
+            )
+        })
+        .map((filtredWorkout) => filtredWorkout.id)
 }
