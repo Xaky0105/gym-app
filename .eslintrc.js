052a558920918d7db696600e68e39e4179eca298
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-10-28 22:20:50 +0300

    add alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,12 +13,42 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+    plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier', 'simple-import-sort'],
     rules: {
         'react/jsx-uses-react': 'off',
         'react/react-in-jsx-scope': 'off',
         'react/display-name': 'off',
         'react/prop-types': 'off',
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
         'prettier/prettier': 'error',
     },
+    overrides: [
+        {
+            files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+            rules: {
+                'simple-import-sort/imports': [
+                    'error',
+                    {
+                        groups: [
+                            // `react` first, `next` second, then packages starting with a character
+                            ['^react$', '^next', '^[a-z]'],
+                            // Packages starting with `@`
+                            ['^@'],
+                            // Packages starting with `~`
+                            ['^~'],
+                            // Imports starting with `../`
+                            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+                            // Imports starting with `./`
+                            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+                            // Style imports
+                            ['^.+\\.s?css$'],
+                            // Side effect imports
+                            ['^\\u0000'],
+                        ],
+                    },
+                ],
+            },
+        },
+    ],
 };
