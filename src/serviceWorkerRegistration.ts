14ae2aa6508eb21ef9494589d6cdedd38a255a56
Xaky <81919274+Xaky0105@users.noreply.github.com>
2022-10-29 22:30:44 +0300

    change manifest

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -18,7 +18,7 @@ const isLocalhost = Boolean(
         window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 );
 
-export function register(config) {
+export function register(config?: any) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
         // The URL constructor is available in all browsers that support SW.
         const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -52,7 +52,7 @@ export function register(config) {
     }
 }
 
-function registerValidSW(swUrl, config) {
+function registerValidSW(swUrl: any, config: any) {
     navigator.serviceWorker
         .register(swUrl)
         .then((registration) => {
@@ -96,7 +96,7 @@ function registerValidSW(swUrl, config) {
         });
 }
 
-function checkValidServiceWorker(swUrl, config) {
+function checkValidServiceWorker(swUrl: any, config: any) {
     // Check if the service worker can be found. If it can't reload the page.
     fetch(swUrl, {
         headers: { 'Service-Worker': 'script' },
